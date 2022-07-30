6e428daeb1acda07435dc54a368480de3572916e
Edwin Alexis <edbwinn@gmail.com>
2022-07-30 15:42:03 -0300

    Documentacion config db

--- db/config/mongoose.js
+++ db/config/mongoose.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
-const connectMongoose = (url) => mongoose.connect(url).then(console.log("congratulations"))
+const connectMongoose = (url) => mongoose.connect(url)
+
+connectMongoose
+    ? console.log("congratulations, we are successfully connected to mongoose")
+    : console.log("Error, could not connect to mongoose")
 
 export { connectMongoose }
--- db/index.js
+++ db/index.js
@@ -1,12 +1,15 @@
-import '#Config/env.js';
+import '#Config/env.js'; // Importa la configuracion de .Env
 import { connectMongoose } from '#Config/mongoose.js';
+// Importa la funcion para conectar mongoose
+
 import { createServerHttp } from '#Config/Http.js';
+
 
 const init = async () => {
 
     await connectMongoose(process.env.MONGODB_URL);
 
-    createServerHttp.listen(3000, () => { console.log(process.env.PORT, process.env.MONGODB_URL) })
+    createServerHttp.listen(process.env.PORT, () => { console.log("Servidor funcionando") })
 }
 
 init()
