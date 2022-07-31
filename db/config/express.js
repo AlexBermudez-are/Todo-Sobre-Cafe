550a8f5afcfdc01cd009fecc75f888170d65c2cd
Edwin Alexis <edbwinn@gmail.com>
2022-07-31 19:52:45 -0300

    config db v3

--- db/config/express.js
+++ db/config/express.js
@@ -1,9 +1,24 @@
 import express from 'express';
 import RouteExpressApp from '../routes/RoutesExpressApp.js';
+import cors from 'cors'
+
 
 const expressApp = express();
-
 expressApp.use(express.json())
+// expressApp.use(cors())
+// expressApp.use(express.urlencoded({ extended: true }))
+
+// expressApp.use(function (req, res, next) {
+//     req.is('json')
+//     req.is('application/json')
+//     req.is('application/*')
+//     req.is('html')
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+//     next();
+// });
 
 expressApp.use('/user', RouteExpressApp)
 
